0b4348320dbd38ff4984a0c21b290569aaadeb32
Vasylchenko Serhii <darnki@gmail.com>
2022-07-05 23:27:40 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('ul#ingredients');
+
+
+const ingredientsElements = ingredients.map((el) => {
+  let res = document.createElement('li');
+  res.textContent = el;
+  res.classList.add('item');
+  return res;
+});
+
+ingredientsListEl.append(...ingredientsElements);
+
+
